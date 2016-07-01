1d1bc9f80645fa02444c37e0844ffd3f584fb4d5
juhav <juha.vilpas@gmail.com>
2016-07-01 04:04:43 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+                        
+            
+        }
+    }
+}
